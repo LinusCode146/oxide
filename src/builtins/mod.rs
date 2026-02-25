fb09bd26149fd6457313d2c734eb89e734df3344
linus.schulze <linuscyber146@gmail.com>
2026-02-25 22:43:41 +0100

    len builtin

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -5,6 +5,7 @@ pub type BuiltinFn = fn(Vec<Object>) -> Object;
 pub fn get_builtin(name: &str) -> Option<Object> {
     match name {
         "log" => Some(Object::Builtin(builtin_log)),
+        "len" => Some(Object::Builtin(builtin_len)),
         _ => None,
     }
 }
@@ -19,4 +20,20 @@ fn builtin_log(args: Vec<Object>) -> Object {
     println!("{}", output.join(" "));
 
     Object::Null
+}
+fn builtin_len(args: Vec<Object>) -> Object {
+    if args.len() != 1 {
+        return Object::Error(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        ));
+    }
+
+    match &args[0] {
+        Object::StringObj(s) => Object::Integer(s.len() as i64),
+        obj => Object::Error(format!(
+            "argument to `len` not supported, got {}",
+            obj.object_type()
+        )),
+    }
 }
